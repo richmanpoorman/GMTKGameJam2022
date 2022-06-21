d3452f60b91ca11c3389b541ccdaac173b16e164
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2022-06-20 22:18:46 -0400

    Examplle

---
+++ TestGitHub/My project/Assets/Bounce.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+
+    public Transform tr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
